abdf811cb503edcb70a39f0f0f01b81c7c6e983e
Haukeys <leopazienza76@gmail.com>
2026-05-13 18:57:18 +0200

    fix an finished globalhandler

--- src/main/java/it/itsacademy/pizzeriaexpress/interceptor/GlobalHandlerException.java
+++ src/main/java/it/itsacademy/pizzeriaexpress/interceptor/GlobalHandlerException.java
@@ -1,16 +1,24 @@
 package it.itsacademy.pizzeriaexpress.interceptor;
 
 import it.itsacademy.pizzeriaexpress.dto.ErroreDTO;
+import it.itsacademy.pizzeriaexpress.exception.BadRequestException;
 import it.itsacademy.pizzeriaexpress.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.NoSuchElementException;
-
+@RestController
 public class GlobalHandlerException {
-
-
-
+    //for check in list or search
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErroreDTO> error404NotFound() {
+        return new ResponseEntity<>(new ErroreDTO(), HttpStatus.NOT_FOUND);
+    }
+    //for ordine
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErroreDTO> error400BadRequest() {
+        return new ResponseEntity<>(new ErroreDTO(), HttpStatus.BAD_REQUEST);
+    }
 
 }
