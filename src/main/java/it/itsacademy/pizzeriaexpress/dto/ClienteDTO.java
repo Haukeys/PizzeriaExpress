b6dfe63c95c6d4e7ebb9ad5c5921b56c837c93cf
Haukeys <leopazienza76@gmail.com>
2026-05-13 10:56:49 +0200

    fix on cliente dto , forget to match with previous change on entities

--- src/main/java/it/itsacademy/pizzeriaexpress/dto/ClienteDTO.java
+++ src/main/java/it/itsacademy/pizzeriaexpress/dto/ClienteDTO.java
@@ -1,9 +1,12 @@
 package it.itsacademy.pizzeriaexpress.dto;
 
+import it.itsacademy.pizzeriaexpress.entity.Ordine;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -14,5 +17,5 @@ public class ClienteDTO {
     private String nome;
     private String Indirizzo;
     private String telefono;
-
+    private List<Ordine> ordini;
 }
